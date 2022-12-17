34c9c9b6dfe21a8453e990ecf6b6dfcaf6c67798
Алина Шахабова <alina.shakhabova@yandex.ru>
2022-12-17 14:53:18 +0300

    module 4 done

--- 4-module/3-task/index.js
+++ 4-module/3-task/index.js
@@ -1,3 +1,23 @@
 function highlight(table) {
-  // ваш код...
+  for (let i = 1; i < table.rows.length; i++) {
+    const currentRow = table.rows[i];
+
+    if (currentRow.cells[3].dataset.available === "true") {
+      currentRow.classList.add("available");
+    } else if (currentRow.cells[3].dataset.available === "false") {
+      currentRow.classList.add("unavailable");
+    } else if (currentRow.cells[3].dataset.available === undefined) {
+      currentRow.hidden = true;
+    }
+
+    if (currentRow.cells[2].textContent === "m") {
+      currentRow.classList.add("male");
+    } else if (currentRow.cells[2].textContent === "f") {
+      currentRow.classList.add("female");
+    }
+
+    if (+currentRow.cells[1].textContent < 18) {
+      currentRow.style.textDecoration = "line-through";
+    }
+  }
 }
